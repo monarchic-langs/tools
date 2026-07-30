ca04c6e443cf646227b465ea67b4ef281341173b
midischwarz12 <midischwarz@proton.me>
2026-07-29 20:50:22 -0500

    feat: add flake checks

--- flake.nix
+++ flake.nix
@@ -3,12 +3,37 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     formatter.${system} = pkgs.alejandra;
     packages.${system}.default = pkgs.gopls;
+    checks = {
+      ${system} = {
+        inherit (self.packages.${system}) default;
+
+        flake-format =
+          pkgs.runCommand "tools-flake-format-check"
+          {nativeBuildInputs = [pkgs.alejandra];}
+          ''
+            alejandra --check ${./flake.nix}
+            touch $out
+          '';
+
+        gopls-smoke =
+          pkgs.runCommand "tools-gopls-smoke"
+          {nativeBuildInputs = [self.packages.${system}.default];}
+          ''
+            gopls version
+            touch $out
+          '';
+      };
+    };
     devShells.${system}.default = pkgs.mkShell {
       packages = [pkgs.gopls pkgs.go];
     };
